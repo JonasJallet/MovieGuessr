090bc4eeb53bf83c47a80ceceb5f4b9bcd9bbe3a
CharleneDR <chdarugna.polytech@gmail.com>
2022-11-25 09:29:59 +0100

    add of the date on api request; modification on database

--- public/assets/js/movies.js
+++ public/assets/js/movies.js
@@ -1,7 +1,7 @@
-function updateText(overview, picture) {
+function updateText(overview, picture, date) {
     document.getElementById('description').innerHTML = overview;
     document.getElementById('image').setAttribute('src', 'https://www.themoviedb.org/t/p/w300_and_h450_bestv2' + picture);
-
+    document.getElementById('date').innerHTML = date;
 }
 
 
@@ -14,7 +14,7 @@ fetch('/api/connect')
             .then(movieSelected => {
                 fetch('https://api.themoviedb.org/3/search/movie?api_key=' + connexion + '&query=' + movieSelected['movie_name'])
                     .then(response => response.json())
-                    .then(movie => updateText(movie['results'][0]['overview'], movie['results'][0]['poster_path']))
+                    .then(movie => updateText(movie['results'][0]['overview'], movie['results'][0]['poster_path'], movie['results'][0]['release_date']))
                     .catch(() => alert('error 1'))
             }
             )
