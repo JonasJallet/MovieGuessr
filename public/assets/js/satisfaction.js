f20adc22b86bf04affb7ef47a5242b53b37f619f
CharleneDR <chdarugna.polytech@gmail.com>
2022-11-27 20:46:11 +0100

    Unique vote implemented

--- public/assets/js/satisfaction.js
+++ public/assets/js/satisfaction.js
@@ -1,32 +1,32 @@
-function liked() {
-    let liked = document.querySelector('.liked')
-    let satisfaction = document.querySelector('.satisfaction');
-    let id = document.querySelector('.liked').alt;
+let liked = document.querySelector('.liked')
+let disliked = document.querySelector('.disliked')
+let satisfaction = document.querySelector('.satisfaction');
+let id = document.querySelector('.liked').alt;
 
-    liked.addEventListener("click", function () {
-        fetch('/liked?id=' + id)
-            .then(response => response.json())
-            .then(satisfaction => console.log(satisfaction))
-        liked.classList.add('likedSelected');
-        satisfaction.classList.remove('liked');
-        satisfaction.classList.remove('disliked');
-    });
-}
+liked.addEventListener("click", likeLocation)
+disliked.addEventListener("click", dislikeLocation)
 
-function dislike() {
-    let disliked = document.querySelector('.disliked')
-    let satisfaction = document.querySelector('.satisfaction');
-    let id = document.querySelector('.liked').alt;
+function likeLocation() {
+    fetch('/liked?id=' + id)
+        .then(response => response.json())
+        .then(satisfaction => console.log(satisfaction))
+    liked.classList.add('likedSelected');
+    disliked.classList.remove('dislikedSelected');
+    satisfaction.classList.remove('liked');
+    satisfaction.classList.remove('disliked');
+    liked.removeEventListener("click", likeLocation)
+    disliked.removeEventListener("click", dislikeLocation)
+};
 
-    disliked.addEventListener("click", function () {
-        fetch('/disliked?id=' + id)
-            .then(response => response.json())
-            .then(satisfaction => console.log(satisfaction))
-        disliked.classList.add('dislikedSelected');
-        satisfaction.classList.remove('liked');
-        satisfaction.classList.remove('disliked');
-    });
-}
+function dislikeLocation() {
+    fetch('/disliked?id=' + id)
+        .then(response => response.json())
+        .then(satisfaction => console.log(satisfaction))
+    disliked.classList.add('dislikedSelected');
+    liked.classList.remove('likedSelected');
+    satisfaction.classList.remove('liked');
+    satisfaction.classList.remove('disliked');
+    liked.removeEventListener("click", likeLocation)
+    disliked.removeEventListener("click", dislikeLocation)
 
-liked();
-dislike();
+};
